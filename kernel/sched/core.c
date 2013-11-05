7523c6fc1b221127db91def6232595dd0353e8d5
Dennis Rassmann <showp1984@gmail.com>
2013-11-05 19:42:14 +0100

    cpuinit: get rid of __cpuinit, first regexp This commit is the result of:
    find -name "*.c" | xargs sed -i "s/ __cpuinit / /g"
    
    Change-Id: I036728a03a287a2cd8a656c8d3a26bc8b044495b
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4958,7 +4958,7 @@ void show_state_filter(unsigned long state_filter)
 		debug_show_all_locks();
 }
 
-void __cpuinit init_idle_bootup_task(struct task_struct *idle)
+void init_idle_bootup_task(struct task_struct *idle)
 {
 	idle->sched_class = &idle_sched_class;
 }
@@ -4971,7 +4971,7 @@ void __cpuinit init_idle_bootup_task(struct task_struct *idle)
  * NOTE: this function does not set the idle thread's NEED_RESCHED
  * flag, to make booting more robust.
  */
-void __cpuinit init_idle(struct task_struct *idle, int cpu)
+void init_idle(struct task_struct *idle, int cpu)
 {
 	struct rq *rq = cpu_rq(cpu);
 	unsigned long flags;
@@ -5516,7 +5516,7 @@ static struct notifier_block __cpuinitdata migration_notifier = {
 	.priority = CPU_PRI_MIGRATION,
 };
 
-static int __cpuinit sched_cpu_active(struct notifier_block *nfb,
+static int sched_cpu_active(struct notifier_block *nfb,
 				      unsigned long action, void *hcpu)
 {
 	switch (action & ~CPU_TASKS_FROZEN) {
@@ -5529,7 +5529,7 @@ static int __cpuinit sched_cpu_active(struct notifier_block *nfb,
 	}
 }
 
-static int __cpuinit sched_cpu_inactive(struct notifier_block *nfb,
+static int sched_cpu_inactive(struct notifier_block *nfb,
 					unsigned long action, void *hcpu)
 {
 	switch (action & ~CPU_TASKS_FROZEN) {
